ec85160052e338338a47f9dd013dbc0d2c65b538
Cheng <lichchchn@gmail.com>
2019-11-29 15:31:46 +0800

    remove return in DPrintf function to avoid warning message

--- src/raft/util.go
+++ src/raft/util.go
@@ -5,9 +5,9 @@ import "log"
 // Debugging
 const Debug = 1
 
-func DPrintf(format string, a ...interface{}) (n int, err error) {
+func DPrintf(format string, a ...interface{}) { // (n int, err error) {
 	if Debug > 0 {
 		log.Printf(format, a...)
 	}
-	return
+	//return
 }
